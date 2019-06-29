d10e6a07407e5faa992251746ba8d2f21a2e9f30
Rajat Mehta <rajatmehta1992@gmail.com>
2019-06-29 20:49:41 +0200

    added data augmentation techniques to the training process.

--- train.py
+++ train.py
@@ -17,7 +17,9 @@ import matplotlib.pyplot as plt
 import time
 import os
 from model import ft_net, ft_net_dense, ft_net_NAS, PCB
-from random_erasing import RandomErasing
+from augmentation import RandomErasing
+from augmentation import ImgAugTransform
+
 import yaml
 import math
 from shutil import copyfile
@@ -43,6 +45,11 @@ parser.add_argument('--batchsize', default=32, type=int, help='batchsize')
 parser.add_argument('--epochs', default=60, type=int, help='epochs')
 parser.add_argument('--stride', default=2, type=int, help='stride')
 parser.add_argument('--erasing_p', default=0, type=float, help='Random Erasing probability, in [0,1]')
+parser.add_argument('--flip', default=0, type=float, help='Horizontal flip probability, in [0,1]')
+parser.add_argument('--rotate', default=0, type=float, help='Rotate images')
+parser.add_argument('--translate', nargs='+', type=float, help='Translate images, give values between 0 and 1')
+parser.add_argument('--aug_comb', action='store_true', help='Use a combination of augmentations')
+parser.add_argument('--scale', nargs='+', type=float, help='Scale images(zoom in or zoom out), give values between 0 and 2')
 parser.add_argument('--use_dense', action='store_true', help='use densenet121')
 parser.add_argument('--use_ftnet', action='store_true', help='use ftnet')
 parser.add_argument('--use_siamese', action='store_true', help='use siamese')
@@ -58,7 +65,7 @@ fp16 = opt.fp16
 data_dir = opt.data_dir
 
 if opt.use_dense is False and opt.use_siamese is False and opt.use_NAS is False and opt.use_ftnet is False:
-    print("No model selectedd. Please select at least one model to train like: use_ftnet or use_siamese")
+    print("No model selected. Please select at least one model to train like: use_ftnet or use_siamese")
     exit()
 
 if opt.use_dense:
@@ -88,16 +95,35 @@ if len(gpu_ids)>0:
 # Load Data
 # ---------
 #
+transform_train_list = []
 
-transform_train_list = [
-        #transforms.RandomResizedCrop(size=128, scale=(0.75,1.0), ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)
-        transforms.Resize((256,128), interpolation=3),
-        transforms.Pad(10),
-        transforms.RandomCrop((256,128)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]
+# transform_train_list = transform_train_list + [# transforms.RandomResizedCrop(size=128, scale=(0.75,1.0),
+# ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)]
+transform_train_list = transform_train_list + [transforms.Resize((256, 128), interpolation=3)]
+# transform_train_list = transform_train_list + [transforms.Pad(10)]
+# transform_train_list = transform_train_list + [transforms.RandomCrop((256,128))]
+
+if opt.flip:
+    transform_train_list = transform_train_list + [transforms.RandomHorizontalFlip(p=opt.flip)]
+
+
+if opt.aug_comb:
+    transform_train_list = transform_train_list + [ImgAugTransform(rt=opt.rotate, tl=opt.translate, scale=opt.scale,
+                                                                   aug_comb=opt.aug_comb)]
+
+else:
+    if opt.rotate:
+        transform_train_list = transform_train_list + [ImgAugTransform(rt=opt.rotate)]
+
+    if opt.translate:
+        transform_train_list = transform_train_list + [ImgAugTransform(tl=opt.translate)]
+
+    if opt.scale:
+        transform_train_list = transform_train_list + [ImgAugTransform(scale=opt.scale)]
+
+transform_train_list = transform_train_list + [transforms.ToTensor()]
+transform_train_list = transform_train_list + [transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
+
 
 transform_val_list = [
         transforms.Resize(size=(256,128),interpolation=3), #Image.BICUBIC
@@ -119,7 +145,7 @@ if opt.PCB:
         ]
 
 if opt.erasing_p>0:
-    transform_train_list = transform_train_list +  [RandomErasing(probability = opt.erasing_p, mean=[0.0, 0.0, 0.0])]
+    transform_train_list = transform_train_list + [RandomErasing(probability = opt.erasing_p, mean=[0.0, 0.0, 0.0])]
 
 if opt.color_jitter:
     transform_train_list = [transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0)] + transform_train_list
@@ -129,6 +155,65 @@ data_transforms = {
     'train': transforms.Compose( transform_train_list ),
     'val': transforms.Compose(transform_val_list),
 }
+
+
+def visualize_dataset_new(dataloaders):
+    """Imshow for Tensor."""
+
+    fig = plt.figure(figsize=(9, 13))
+    columns = 4
+    rows = 5
+    input, label = next(iter(dataloaders['train']))
+
+    fig.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.5, hspace=0.5)
+    # prep (x,y) for extra plotting
+    xs = np.linspace(0, 2 * np.pi, 60)  # from 0 to 2pi
+    ys = np.abs(np.sin(xs))  # absolute of sine
+
+    # ax enables access to manipulate each of subplots
+    ax = []
+    j=0
+
+    for i in range(columns * rows):
+        img = input[j]
+        lbl = label[j].item()
+        img = img.permute(1, 2, 0)
+        img = img.numpy()
+        # create subplot and append to ax
+        ax.append(fig.add_subplot(rows, columns, i + 1))
+        ax[-1].set_title(str(i))  # set title
+        plt.imshow(img)
+        j += 1
+
+    # do extra plots on selected axes/subplots
+    # note: index starts with 0
+    ax[2].plot(xs, 3 * ys)
+    ax[19].plot(ys ** 2, xs)
+
+    plt.savefig("./model/ft_ResNet/dataset.png")
+
+
+def visualize_dataset(dataloaders):
+    """Imshow for Tensor."""
+
+    fig = plt.figure(figsize=(8, 8))
+    columns = 4
+    rows = 5
+    j = 0
+    for i in range(1, columns * rows + 1):
+        for batch_idx, (inputs, labels) in enumerate(dataloaders['train']):
+            inputs = inputs
+            break
+
+        img = inputs[j]
+        label = labels[j]
+        img = img.permute(1, 2, 0)
+        img = img.numpy()
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(img)
+        j+=1
+    plt.show()
+    plt.savefig("./model/ft_ResNet/ft_ResNet_HFlip/dataset.png")
 
 
 train_all = ''
@@ -144,6 +229,9 @@ image_datasets['val'] = datasets.ImageFolder(os.path.join(data_dir, 'val'),
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
                                              shuffle=True, num_workers=8, pin_memory=True) # 8 workers may work faster
               for x in ['train', 'val']}
+
+visualize_dataset_new(dataloaders)
+
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
 
@@ -172,18 +260,19 @@ y_err = {}
 y_err['train'] = []
 y_err['val'] = []
 
+
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
-    #best_model_wts = model.state_dict()
-    #best_acc = 0.0
+    # best_model_wts = model.state_dict()
+    # best_acc = 0.0
     warm_up = 0.1 # We start from the 0.1*lrRate
     warm_iteration = round(dataset_sizes['train']/opt.batchsize)*opt.warm_epoch # first 5 epoch
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
-        
+
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
@@ -201,7 +290,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 now_batch_size,c,h,w = inputs.shape
                 if now_batch_size<opt.batchsize: # skip the last batch
                     continue
-                #print(inputs.shape)
+                # print(inputs.shape)
                 # wrap them in Variable
                 if use_gpu:
                     inputs = Variable(inputs.cuda().detach())
@@ -212,7 +301,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 # if we use low precision, input also need to be fp16
                 #if fp16:
                 #    inputs = inputs.half()
- 
+
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
@@ -233,7 +322,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                     for i in range(num_part):
                         part[i] = outputs[i]
 
-                    score = sm(part[0]) + sm(part[1]) +sm(part[2]) + sm(part[3]) +sm(part[4]) +sm(part[5])
+                    score = sm(part[0]) + sm(part[1]) + sm(part[2]) + sm(part[3]) + sm(part[4]) + sm(part[5])
                     _, preds = torch.max(score.data, 1)
 
                     loss = criterion(part[0], labels)
@@ -241,7 +330,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                         loss += criterion(part[i+1], labels)
 
                 # backward + optimize only if in training phase
-                if epoch<opt.warm_epoch and phase == 'train': 
+                if epoch<opt.warm_epoch and phase == 'train':
                     warm_up = min(1.0, warm_up + 0.9 / warm_iteration)
                     loss *= warm_up
 
@@ -262,12 +351,12 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
-            
+
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
-            
+
             y_loss[phase].append(epoch_loss)
-            y_err[phase].append(1.0-epoch_acc)            
+            y_err[phase].append(1.0-epoch_acc)
             # deep copy the model
             if phase == 'val':
                 last_model_wts = model.state_dict()
@@ -293,11 +382,13 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
 ######################################################################
 # Draw Curve
-#---------------------------
+# ---------------------------
 x_epoch = []
 fig = plt.figure()
 ax0 = fig.add_subplot(121, title="loss")
 ax1 = fig.add_subplot(122, title="top1err")
+
+
 def draw_curve(current_epoch):
     x_epoch.append(current_epoch)
     ax0.plot(x_epoch, y_loss['train'], 'bo-', label='train')
@@ -311,7 +402,9 @@ def draw_curve(current_epoch):
 
 ######################################################################
 # Save model
-#---------------------------
+# ---------------------------
+
+
 def save_network(network, epoch_label):
     save_filename = 'net_%s.pth'% epoch_label
     save_path = os.path.join('./model',name,save_filename)
@@ -371,16 +464,16 @@ if not opt.PCB:
              {'params': model.classifier.parameters(), 'lr': opt.lr}
          ], weight_decay=5e-4, momentum=0.9, nesterov=True)
 else:
-    ignored_params = list(map(id, model.model.fc.parameters() ))
-    ignored_params += (list(map(id, model.classifier0.parameters() )) 
-                     +list(map(id, model.classifier1.parameters() ))
-                     +list(map(id, model.classifier2.parameters() ))
-                     +list(map(id, model.classifier3.parameters() ))
-                     +list(map(id, model.classifier4.parameters() ))
-                     +list(map(id, model.classifier5.parameters() ))
-                     #+list(map(id, model.classifier6.parameters() ))
-                     #+list(map(id, model.classifier7.parameters() ))
-                      )
+    ignored_params = list(map(id, model.model.fc.parameters()))
+    ignored_params += (list(map(id, model.classifier0.parameters()))
+                     + list(map(id, model.classifier1.parameters()))
+                     + list(map(id, model.classifier2.parameters()))
+                     + list(map(id, model.classifier3.parameters()))
+                     + list(map(id, model.classifier4.parameters()))
+                     + list(map(id, model.classifier5.parameters()))
+                     # +list(map(id, model.classifier6.parameters()))
+                     # +list(map(id, model.classifier7.parameters()))
+                       )
     base_params = filter(lambda p: id(p) not in ignored_params, model.parameters())
     optimizer_ft = optim.SGD([
              {'params': base_params, 'lr': 0.1*opt.lr},
@@ -391,8 +484,8 @@ else:
              {'params': model.classifier3.parameters(), 'lr': opt.lr},
              {'params': model.classifier4.parameters(), 'lr': opt.lr},
              {'params': model.classifier5.parameters(), 'lr': opt.lr},
-             #{'params': model.classifier6.parameters(), 'lr': 0.01},
-             #{'params': model.classifier7.parameters(), 'lr': 0.01}
+             # {'params': model.classifier6.parameters(), 'lr': 0.01},
+             # {'params': model.classifier7.parameters(), 'lr': 0.01}
          ], weight_decay=5e-4, momentum=0.9, nesterov=True)
 
 # Decay LR by a factor of 0.1 every 40 epochs
@@ -402,7 +495,7 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=40, gamma=0.1)
 # Train and evaluate
 # ^^^^^^^^^^^^^^^^^^
 #
-# It should take around 1-2 hours on GPU. 
+# It should take around 1-2 hours on GPU.
 #
 save_train_config_files()
 
